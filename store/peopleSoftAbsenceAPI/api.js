78f39a52507d69f61f91e906ba39da9f45f537e2
shahraiz-cb <shahraiz.ali@crowdbotics.com>
2023-07-28 23:10:40 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/peopleSoftAbsenceAPI/api.js
+++ store/peopleSoftAbsenceAPI/api.js
@@ -1,11 +1,16 @@
-import axios from "axios"
+import axios from "axios";
 const peopleSoftAbsenceAPI = axios.create({
   baseURL: "https://10.12.13.4/PSIGW/RESTListeningConnector/PSFT",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function peoplesoftabsenceapi_post_request_createabsence_create(payload) {
-  return peopleSoftAbsenceAPI.post(`/request/createabsence`)
+  return peopleSoftAbsenceAPI.post(`/request/createabsence`);
 }
+
 export const apiService = {
   peoplesoftabsenceapi_post_request_createabsence_create
-}
+};
