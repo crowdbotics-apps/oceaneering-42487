4b9a72e5cf391adf9fd26d71d6f55442e9f20b39
Crowdbotics <team@crowdbotics.com>
2023-07-31 18:38:47 +0000

    Update admin title

--- backend/peoplesoft_demo_42487/urls.py
+++ backend/peoplesoft_demo_42487/urls.py
@@ -34,9 +34,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "oceaneering-demo"
-admin.site.site_title = "oceaneering-demo Admin Portal"
-admin.site.index_title = "oceaneering-demo Admin"
+admin.site.site_header = "oceaneering"
+admin.site.site_title = "oceaneering Admin Portal"
+admin.site.index_title = "oceaneering Admin"
 
 # swagger
 urlpatterns += [
